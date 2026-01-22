2d02e3b6b426571b82283b3b96413584d00a7830
LuizOC22 <luiz.lr901@gmail.com>
2026-01-22 20:54:16 -0300

    add spring security

---
+++ src/main/java/com/luiz/biblioteca_pessoal/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.luiz.biblioteca_pessoal.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+
+                        .requestMatchers("/", "/index.html", "/style.css").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/livros").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/indicacoes").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/indicacoes").permitAll()
+
+                        .anyRequest().authenticated()
+                    )
+                .formLogin(form -> form
+                                .defaultSuccessUrl("/admin.html", true)
+                                .permitAll()
+                        )
+                .httpBasic(basic -> {});
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("luiz")
+                .password("Oliver%12")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin);
+    }
+
+}
